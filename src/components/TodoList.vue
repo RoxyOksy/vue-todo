0aa70d50779d95330cdda4983d5f93e765bccae1
RoxyOksy <roksana.dp.ua@gmail.com>
2020-11-07 16:02:09 +0200

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Completed tasks: {{ todos.filter(todo => todo.done === true).length }}</p>
+    <p>Pending tasks: {{ todos.filter(todo => todo.done === false).length }}</p>
+    <ul>
+      <li>TODO A</li>
+      <li>TODO B</li>
+      <li>TODO C</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TodoList',
+  props: ['todos']
+})
+</script>
+<style scoped lang="less">
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px;
+}
+
+</style>
